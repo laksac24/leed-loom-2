e7727a8ba5aaf1cc935a4729e90988e0c2ca2fae
laksac24 <lakshyasachdeva24@gmail.com>
2025-07-06 19:00:33 +0530

    initial commit

--- build.sh
+++ build.sh
@@ -2,7 +2,26 @@
 # exit on error
 set -o errexit
 
+# Update package lists
+apt-get update
+
+# Install system dependencies for lxml and other packages
+apt-get install -y \
+    build-essential \
+    libxml2-dev \
+    libxslt1-dev \
+    libffi-dev \
+    libssl-dev \
+    zlib1g-dev \
+    libjpeg-dev \
+    libpng-dev \
+    wget \
+    curl \
+    unzip \
+    gnupg
+
 # Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
 # Install Chrome
@@ -13,7 +32,7 @@ apt-get install -y google-chrome-stable
 
 # Install ChromeDriver
 CHROMEDRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
-wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/\$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
+wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
 unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
 chmod +x /usr/local/bin/chromedriver
 
